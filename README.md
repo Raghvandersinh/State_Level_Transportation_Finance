65a9ca1d221e24c444341aa56b0d117b8b7f9fd6
Raghvandersinh <89419757+Raghvandersinh@users.noreply.github.com>
2026-09-25 12:29:01 -0400

    Added queries to get total expenditure per capita in 2024, 2022, and 2020. Still deciding if I should just focus on 2024 or do an Covid prime and post covid comparison.

---
+++ README.md
@@ -0,0 +1,3 @@
+# State Air Transportation Comparison Analysis
+
+## We will focus on Total Revenue/Expenditure, per capita comparison, and Revenue vs. Expenditure balance.
--- queries.sql
+++ queries.sql
@@ -5,4 +5,44 @@ GROUP BY mode;
 
 SELECT DISTINCT mode, year
 FROM State_Level_Transportation_Finance.transportation_state_finanace
-where mode = 'Air'
+where mode = 'Air';
+
+/*Get total expenditure by state 2024(post covid)*/
+Select year, cash_flow, mode, state, state_code, 
+SUM(chained_per_pop) as total_expenditure_per_state,
+DENSE_RANK() OVER(ORDER BY SUM(chained_per_pop) DESC) as total_spend_rank
+from State_Level_Transportation_Finance.transportation_state_finanace
+Where cash_flow = 'Expenditure' and mode = 'Air' and year = '2,024'
+and gov_level = 'State and Local'
+GROUP BY 1,2,3,4,5
+ORDER BY total_spend_rank;
+
+/*Get total expenditure by state 2020(Start covid)*/
+Select year, cash_flow, mode, state, state_code, 
+SUM(chained_per_pop) as total_expenditure_per_state,
+DENSE_RANK() OVER(ORDER BY SUM(chained_per_pop) DESC) as total_spend_rank
+from State_Level_Transportation_Finance.transportation_state_finanace
+Where cash_flow = 'Expenditure' and mode = 'Air' and year = '2,020'
+and gov_level = 'State and Local'
+GROUP BY 1,2,3,4,5
+ORDER BY total_spend_rank;
+
+/*Get total expenditure by state 2022(Prime covid)*/
+Select year, cash_flow, mode, state, state_code, 
+SUM(chained_per_pop) as total_expenditure_per_state,
+DENSE_RANK() OVER(ORDER BY SUM(chained_per_pop) DESC) as total_spend_rank
+from State_Level_Transportation_Finance.transportation_state_finanace
+Where cash_flow = 'Expenditure' and mode = 'Air' and year = '2,022'
+and gov_level = 'State and Local'
+GROUP BY 1,2,3,4,5
+ORDER BY total_spend_rank;
+
+-- Test
+
+Select *
+-- DENSE_RANK() OVER(ORDER BY SUM(chained_per_pop) DESC) as total_spend_rank
+from State_Level_Transportation_Finance.transportation_state_finanace
+Where cash_flow = 'Expenditure' and mode = 'Air' and year = '2,024'
+and gov_level = 'State and Local';
+-- GROUP BY 1,2,3,4,5;
+-- ORDER BY total_spend_rank;
